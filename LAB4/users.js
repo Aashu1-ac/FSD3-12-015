3f03d7303f57eae770aa3185ff72d2fe326bc3e6
Aashu1-ac <aashu.25b01011221@abes.ac.in>
2026-09-11 15:49:40 +0530

    https://github.com/Aashu1-ac/FSD3-12-015.git

---
+++ LAB4/users.js
@@ -0,0 +1,10 @@
+// we use inmemory database 
+let users =[
+    {id:1, name:"Aashu Chauhan", mob:"7078123456", email:"aashu@example.com"},
+    {id:2, name:"Shalini Chauhan", mob:"7078123456", email:"shalini@example.com"}
+];
+
+
+let nextId = 3;
+
+export const getUsers = () => users;
